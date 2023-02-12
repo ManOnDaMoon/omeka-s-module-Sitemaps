ff6927bbbd15c9bf81c56b95a534ca668fb9b9b2
ManOnDaMoon <manondamoon@gmail.com>
2023-02-12 21:42:45 +0100

    Fetch last modification date for site pages and item sets index files

--- src/Controller/Site/SitemapsController.php
+++ src/Controller/Site/SitemapsController.php
@@ -29,39 +29,68 @@ class SitemapsController extends AbstractActionController {
         }
         
         // Sitemap #1 is for pages and item sets. Check if there are some
-        $pages = array_merge($site->linkedPages(), $site->notlinkedPages());
-        
+        // Fetch pages
         $query = array();
         $query['site_id'] = $site->id();
+        $query['sort_by'] = 'modified';
+        $query['sort_order'] = 'desc';
+        $query['limit'] = 1;
+        $response = $this->api()->search('site_pages', $query);
+        $pagesCount = $response->getTotalResults();
+        if ($content = $response->getContent()) {
+            $lastModPage = $content[0];
+        } else {
+            $lastModPage = null;
+        }
+
+        // Fetch item sets
+        $query = array();
+        $query['site_id'] = $site->id();
+        $query['sort_by'] = 'modified';
+        $query['sort_order'] = 'desc';
+        $query['limit'] = 1;
         $response = $this->api()->search('item_sets', $query);
-        $response->getTotalResults();
-        $itemsets = $response->getContent();
+        $itemsetsCount = $response->getTotalResults();
+        if ($content = $response->getContent()) { // Sites can exist without attached item sets.
+            $lastModItemSet = $content[0];
+        } else {
+            $lastModItemSet = null;
+        }
         
-        if (count($pages) > 0 || count($itemsets) > 0) {
-            $sitemaps[] = ['url' => $site->siteUrl($site->slug(), true) . '/sitemap-1.xml',
-                'lastmod' => '' // TODO : Get max lastmod of pages and itemsets
-            ];
+        $sitemapsCount = 0;
+        if ($lastModPage && $lastModItemSet) {
+                $lastMod = max([$lastModPage->modified(), $lastModItemSet->modified()]);
+        } else if ($lastModPage) {
+                $lastMod = $lastModPage->modified(); // FIXME : Breaks if there is no page on site (which can happen)
+        } else if ($lastModItemSet) {
+            $lastMod = $lastModItemSet->modified();
+        }
+            
+        if ($pagesCount + $itemsetsCount > 0) {
+                $sitemaps[] = ['url' => $site->siteUrl($site->slug(), true) . '/sitemap-1.xml',
+                    'lastmod' => $lastMod->format('Y-m-d')
+                ];
+                $sitemapsCount++;
         }
         
         // Sitemap #2 and next are for items
         $query = array();
         $query['site_id'] = $site->id();
-        $query['limit'] = 0;
+        $query['limit'] = 0; // Just get the total count
         $response = $this->api()->search('items', $query);
         $itemsCount = $response->getTotalResults();
         
-        $sitemapsCount = intval($itemsCount / 200) + (($itemsCount % 200) > 0 ? 1 : 0); // TODO fetch limit from setting
+        $itemsSitemapsCount = intval($itemsCount / 200) + (($itemsCount % 200) > 0 ? 1 : 0); // TODO fetch limit from setting
         
-        for ($i  = 2; $i <= $sitemapsCount + 1; $i++) {
+        for ($i  = $sitemapsCount + 1; $i <= $itemsSitemapsCount + $sitemapsCount ; $i++) {
             $sitemaps[] = ['url' => $site->siteUrl($site->slug(), true) . '/sitemap-' . $i . '.xml',
-                'lastmod' => '' // TODO : Get max lastmod of pages and itemsets
+                'lastmod' => '' // TODO : Get max lastmod of pages and itemsets. To do so, add sort_by modified and sort_order desc to query
             ];
         }
         
         /** @var \Laminas\View\Model\ViewModel $view */
         $view = new ViewModel();
         $view->setTemplate('site/sitemap-index');
-        
         $view->setVariable('sitemaps', $sitemaps);
         $view->setTerminal(true);
         
@@ -69,6 +98,8 @@ class SitemapsController extends AbstractActionController {
     }
     
     public function sitemapAction() {
+        
+        // TODO : Add sort_by 'modified' and sort_order 'desc' to query
         
         $site = $this->currentSite();
         $sitemapPage = $this->params('sitemap-page');
@@ -96,7 +127,9 @@ class SitemapsController extends AbstractActionController {
         
         if (!$hasIndex) {
             // Simple Sitemap file with all content
-            $pages = array_merge($site->linkedPages(), $site->notlinkedPages());
+            // TODO: use API to get pages and item sets
+            
+            $pages = $site->pages();
             
             $query = array();
             $query['site_id'] = $site->id();
@@ -118,7 +151,7 @@ class SitemapsController extends AbstractActionController {
             if ($sitemapPage == "1") {
                 
                 // Fetch site pages
-                $pages = array_merge($site->linkedPages(), $site->notlinkedPages());
+                $pages = $site->pages();
                 
                 // Fetch site item sets
                 $query = array();
